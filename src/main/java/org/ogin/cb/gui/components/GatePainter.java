7204f1f12bb5faa336d311b5c2ed6145462281f9
rogin <rogin@users.noreply.github.com>
2019-06-24 21:49:16 -0500

    move "Not" painting into GatePainter

--- src/main/java/org/ogin/cb/gui/components/GatePainter.java
+++ src/main/java/org/ogin/cb/gui/components/GatePainter.java
@@ -2,8 +2,10 @@ package org.ogin.cb.gui.components;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
 
 import javax.swing.JComponent;
 
@@ -135,5 +137,41 @@ public final class GatePainter {
         if(includeNotIndicator) {
             paintNotIndicator(g2, xPoints[2], yPoints[2]);
         }
+	}
+
+	public static void paintNot(Graphics g, JComponent component) {
+        Graphics2D g2 = (Graphics2D)g;
+
+        Rectangle bounds = component.getBounds();
+        
+        //three tips of triangle
+        Point top = new Point(5, 5);
+        Point right = new Point(bounds.width -5, bounds.height / 2);
+        Point bottom = new Point(5, bounds.height-5);
+        
+        g2.drawLine((int)top.getX(), (int)top.getY(), (int)right.getX(), (int)right.getY());
+        g2.draw(new Line2D.Float(top, right));
+        g2.draw(new Line2D.Float(right, bottom));
+        g2.draw(new Line2D.Float(bottom, top));
+
+        //the "NOT" circle
+        g2.drawOval((int)right.getX()-3, (int)right.getY()-3, 6, 6);
+        //Shape round = new RoundRectangle2D.Float((int)right.getX()-3, (int)right.getY()-3, 6, 6, 5, 25);
+        //g2.draw(round);
+
+        /*
+        //we can also draw a path
+        int xPoints[] = {5, bounds.width-5, 5};
+        int yPoints[] = {5, bounds.height/2, bounds.height-5};
+
+        GeneralPath triangle = new GeneralPath();
+
+        triangle.moveTo(xPoints[0], yPoints[0]);
+        for (int i = 1; i < xPoints.length; i++) {
+            triangle.lineTo(xPoints[i], yPoints[i]);
+        }
+        triangle.closePath();
+        g2.draw(triangle);
+        */
 	}
 }
